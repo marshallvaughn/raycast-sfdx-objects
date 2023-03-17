142cf771fd2a9ecc35e8ee8073c4238b2000ac61
Marshall Vaughn <5757870+marshallvaughn@users.noreply.github.com>
2023-03-17 19:55:42 -0400

    format with prettier

--- src/index.tsx
+++ src/index.tsx
@@ -4,30 +4,30 @@ import { QueryResult } from "jsforce";
 import { AuthInfo, Connection, ConfigAggregator, OrgConfigProperties } from "@salesforce/core";
 import { EntityDefinition, State, SetupUrlMapItem, SetupSubpath } from "./types";
 
-process.env.SFDX_USE_GENERIC_UNIX_KEYCHAIN = 'true';
+process.env.SFDX_USE_GENERIC_UNIX_KEYCHAIN = "true";
 
 const cache = new Cache();
 
-const file = { baseUrl: '' };
+const file = { baseUrl: "" };
 const fieldKeys = [
-  'Id',
-  'KeyPrefix',
-  'Description',
-  'DeveloperName',
-  'QualifiedApiName',
-  'IsCustomizable',
-  'DurableId',
-  'EditDefinitionUrl',
-  'EditUrl',
-  'NewUrl',
-  'DetailUrl',
-  'MasterLabel',
-  'NamespacePrefix',
-  'PluralLabel'
+  "Id",
+  "KeyPrefix",
+  "Description",
+  "DeveloperName",
+  "QualifiedApiName",
+  "IsCustomizable",
+  "DurableId",
+  "EditDefinitionUrl",
+  "EditUrl",
+  "NewUrl",
+  "DetailUrl",
+  "MasterLabel",
+  "NamespacePrefix",
+  "PluralLabel",
 ];
 
 const soqlLimit = 500;
-const soqlQuery = `SELECT ${fieldKeys.join(', ')} 
+const soqlQuery = `SELECT ${fieldKeys.join(", ")} 
 FROM EntityDefinition 
 WHERE IsLayoutable = TRUE
 ORDER BY QualifiedApiName, KeyPrefix, NamespacePrefix LIMIT ${soqlLimit}`;
@@ -42,8 +42,8 @@ export default function Command(): JSX.Element {
           setState({ items: result.records });
         } catch (error) {
           setState({
-            error: error instanceof Error ? error : new Error("Something went wrong")
-          })
+            error: error instanceof Error ? error : new Error("Something went wrong"),
+          });
         }
       } else {
         const cached = cache.get("items");
@@ -66,121 +66,122 @@ export default function Command(): JSX.Element {
 
 const setupUrlMap: SetupUrlMapItem[] = [
   {
-    name: 'Details',
-    label: 'Details',
-    icon: Icon.Info
+    name: "Details",
+    label: "Details",
+    icon: Icon.Info,
   },
   {
-    name: 'FieldsAndRelationships',
-    label: 'Fields & Relationships',
-    icon: Icon.Filter
+    name: "FieldsAndRelationships",
+    label: "Fields & Relationships",
+    icon: Icon.Filter,
   },
   {
-    name: 'Layouts',
-    label: 'Layouts',
-    icon: Icon.AppWindowList
+    name: "Layouts",
+    label: "Layouts",
+    icon: Icon.AppWindowList,
   },
   {
-    name: 'LightningPages',
-    label: 'Lightning Pages',
-    icon: Icon.AppWindowGrid3x3
+    name: "LightningPages",
+    label: "Lightning Pages",
+    icon: Icon.AppWindowGrid3x3,
   },
   {
-    name: 'Limits',
-    label: 'Limits',
-    icon: Icon.Gauge
+    name: "Limits",
+    label: "Limits",
+    icon: Icon.Gauge,
   },
   {
-    name: 'Triggers',
-    label: 'Triggers',
-    icon: Icon.Bolt
+    name: "Triggers",
+    label: "Triggers",
+    icon: Icon.Bolt,
   },
   {
-    name: 'FlowTriggers',
-    label: 'Flow Triggers',
-    icon: Icon.Bolt
-  }
-]
+    name: "FlowTriggers",
+    label: "Flow Triggers",
+    icon: Icon.Bolt,
+  },
+];
 
-function RecordListItem(props: { item: EntityDefinition, index: number }) {
+function RecordListItem(props: { item: EntityDefinition; index: number }) {
   return (
     <List.Item
       title={props.item.QualifiedApiName}
       icon={Icon.Box}
       // subtitle={props.item.MasterLabel}
       id={props.item.DurableId}
-      keywords={[props.item.KeyPrefix, props.item.QualifiedApiName, props.item.DurableId, props.item.DeveloperName, props.item.MasterLabel, props.item.PluralLabel]}
+      keywords={[
+        props.item.KeyPrefix,
+        props.item.QualifiedApiName,
+        props.item.DurableId,
+        props.item.DeveloperName,
+        props.item.MasterLabel,
+        props.item.PluralLabel,
+      ]}
       accessories={[
         {
           text: props.item.KeyPrefix,
           // icon: Icon.Key,
-          tooltip: `KeyPrefix for ${props.item.QualifiedApiName} SObject`
-        }
+          tooltip: `KeyPrefix for ${props.item.QualifiedApiName} SObject`,
+        },
       ]}
       detail={
         <List.Item.Detail
           markdown={props.item.Description}
           metadata={
             <List.Item.Detail.Metadata>
-              <List.Item.Detail.Metadata.Link title={`${props.item.QualifiedApiName} Setup`} text="Go to Setup" target={getSetupUrl(props.item)} />
+              <List.Item.Detail.Metadata.Link
+                title={`${props.item.QualifiedApiName} Setup`}
+                text="Go to Setup"
+                target={getSetupUrl(props.item)}
+              />
 
               <List.Item.Detail.Metadata.Separator />
 
-              <List.Item.Detail.Metadata.Label title="KeyPrefix" text={props.item.KeyPrefix || '–'} />
+              <List.Item.Detail.Metadata.Label title="KeyPrefix" text={props.item.KeyPrefix || "–"} />
 
               <List.Item.Detail.Metadata.Separator />
 
-              <List.Item.Detail.Metadata.Label title="QualifiedApiName" text={props.item.QualifiedApiName || '–'} />
-              <List.Item.Detail.Metadata.Label title="MasterLabel" text={props.item.MasterLabel || '–'} />
-              <List.Item.Detail.Metadata.Label title="PluralLabel" text={props.item.PluralLabel || '–'} />
+              <List.Item.Detail.Metadata.Label title="QualifiedApiName" text={props.item.QualifiedApiName || "–"} />
+              <List.Item.Detail.Metadata.Label title="MasterLabel" text={props.item.MasterLabel || "–"} />
+              <List.Item.Detail.Metadata.Label title="PluralLabel" text={props.item.PluralLabel || "–"} />
 
               <List.Item.Detail.Metadata.Separator />
 
-              <List.Item.Detail.Metadata.Label title="Description" text={props.item.Description || '–'} />
+              <List.Item.Detail.Metadata.Label title="Description" text={props.item.Description || "–"} />
 
               <List.Item.Detail.Metadata.Separator />
 
-              <List.Item.Detail.Metadata.Label title="NamespacePrefix" text={props.item.NamespacePrefix || '–'} />
-              <List.Item.Detail.Metadata.Label title="DeveloperName" text={props.item.DeveloperName || '–'} />
-              <List.Item.Detail.Metadata.Label title="DurableId" text={props.item.DurableId || '–'} />
+              <List.Item.Detail.Metadata.Label title="NamespacePrefix" text={props.item.NamespacePrefix || "–"} />
+              <List.Item.Detail.Metadata.Label title="DeveloperName" text={props.item.DeveloperName || "–"} />
+              <List.Item.Detail.Metadata.Label title="DurableId" text={props.item.DurableId || "–"} />
 
               <List.Item.Detail.Metadata.Separator />
 
-              <List.Item.Detail.Metadata.Label title="DetailUrl" text={props.item.DetailUrl || '–'} />
-              <List.Item.Detail.Metadata.Label title="EditUrl" text={props.item.EditUrl || '–'} />
-              <List.Item.Detail.Metadata.Label title="NewUrl" text={props.item.NewUrl || '–'} />
+              <List.Item.Detail.Metadata.Label title="DetailUrl" text={props.item.DetailUrl || "–"} />
+              <List.Item.Detail.Metadata.Label title="EditUrl" text={props.item.EditUrl || "–"} />
+              <List.Item.Detail.Metadata.Label title="NewUrl" text={props.item.NewUrl || "–"} />
             </List.Item.Detail.Metadata>
           }
         />
       }
-
       actions={
-        <ActionPanel title={'Actions'}>
+        <ActionPanel title={"Actions"}>
           <ActionPanel.Section title={`${props.item.QualifiedApiName} Setup`}>
-            {
-              setupUrlMap.map((item) => {
-                return <Action.OpenInBrowser title={item.label} url={getSetupUrl(props.item, item.name)} icon={item.icon} />
-              })
-            }
+            {setupUrlMap.map((item) => {
+              return (
+                <Action.OpenInBrowser title={item.label} url={getSetupUrl(props.item, item.name)} icon={item.icon} />
+              );
+            })}
           </ActionPanel.Section>
           <ActionPanel.Section title="Copy">
-            <Action.CopyToClipboard
-              title="Copy QualifiedApiName"
-              content={props.item.QualifiedApiName}
-            />
-            <Action.CopyToClipboard
-              title="Copy MasterLabel"
-              content={props.item.MasterLabel}
-            />
-            <Action.CopyToClipboard
-              title="Copy KeyPrefix"
-              content={props.item.KeyPrefix}
-            />
+            <Action.CopyToClipboard title="Copy QualifiedApiName" content={props.item.QualifiedApiName} />
+            <Action.CopyToClipboard title="Copy MasterLabel" content={props.item.MasterLabel} />
+            <Action.CopyToClipboard title="Copy KeyPrefix" content={props.item.KeyPrefix} />
           </ActionPanel.Section>
         </ActionPanel>
       }
     />
-  )
+  );
 }
 
 async function getDefaultDevHubUsername() {
@@ -195,15 +196,17 @@ async function getDefaultDevHubUsername() {
 }
 
 function getSetupUrl(entity: EntityDefinition, setupSubpath?: SetupSubpath): string {
-  setupSubpath = setupSubpath || 'Details'
-  return `${file.baseUrl}/lightning/setup/ObjectManager/${entity.QualifiedApiName}/${setupSubpath}/view`
+  setupSubpath = setupSubpath || "Details";
+  return `${file.baseUrl}/lightning/setup/ObjectManager/${entity.QualifiedApiName}/${setupSubpath}/view`;
 }
 
 async function getConnection(): Promise<Connection> {
   const cachedConnection = cache.get("connection");
   if (!cachedConnection) {
     const sfdxUsername = await getDefaultDevHubUsername();
-    const connection: Connection = await Connection.create({ authInfo: await AuthInfo.create({ username: sfdxUsername }) });
+    const connection: Connection = await Connection.create({
+      authInfo: await AuthInfo.create({ username: sfdxUsername }),
+    });
     cache.set("connection", JSON.stringify(connection));
     return connection;
   } else {
@@ -211,14 +214,20 @@ async function getConnection(): Promise<Connection> {
   }
 }
 
+async function getSchema(): Promise<QueryResult<EntityDefinition>> {
+  const cachedResponse = cache.get("response");
+  if (!cachedResponse) {
+    return await runQuery();
+  } else {
+    return JSON.parse(cachedResponse) as QueryResult<EntityDefinition>;
+  }
+}
+
 async function runQuery(): Promise<QueryResult<EntityDefinition>> {
   const connection: Connection = await getConnection();
-  // const sfdxUsername = await getDefaultDevHubUsername();
-  // const connection = await Connection.create({ authInfo: await AuthInfo.create({ username: sfdxUsername }) });
-  // cache.set("connection", JSON.stringify(connection));
   file.baseUrl = connection._baseUrl();
-  file.baseUrl = file.baseUrl.replace(/.com\/.*/g, '.com')
+  file.baseUrl = file.baseUrl.replace(/.com\/.*/g, ".com");
   const response = await connection.tooling.query(soqlQuery);
-  cache.set("items", JSON.stringify(response));
+  cache.set("response", JSON.stringify(response));
   return await connection.tooling.query(soqlQuery);
 }
